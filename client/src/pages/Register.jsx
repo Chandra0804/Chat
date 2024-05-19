3350ce95cef5925cab57decf202c1a32e8a69b7c
Chandra0804 <chandrasaiteja0804@gmail.com>
2024-05-20 00:23:45 +0530

    register issue

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
   const navigate = useNavigate();
